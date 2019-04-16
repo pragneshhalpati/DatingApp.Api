4092a6cf6ea9b302a9b4e5f3b72f722a6e53063f
Pragnesh <pragnesh@evonious.com>
2019-04-16 17:12:56 +0530

    End of section 4

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DattingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
